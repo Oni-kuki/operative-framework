9b3b7aa8b2f503bec17773f2f7a63dd0ef12bc94
Oni-kuki <88449481+Oni-kuki@users.noreply.github.com>
2022-02-18 14:24:52 +0100

    change version of goinsta

--- modules/instagram/instagram_following.go
+++ modules/instagram/instagram_following.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/graniet/go-pretty/table"
 	"github.com/graniet/operative-framework/session"
-	"gopkg.in/ahmdrz/goinsta.v2"
+	"gopkg.in/ahmdrz/goinsta.v3"
 )
 
 type InstagramFollowing struct {
